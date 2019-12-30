38c7d5b7e7cea07c9d958a57bab2d5e4e8458ad7
Kosogov Elias <i.kosogov@gmail.com>
2019-12-30 15:39:22 +0200

    created model Client created model Order

---
+++ app/src/main/java/com/example/client/Models/Client.java
@@ -0,0 +1,51 @@
+package com.example.client.Models;
+
+import java.io.Serializable;
+
+public class Client implements Serializable {
+    public String UserId;
+    public String nameSurnameNewClient;
+    public int numPhoneNewClient;
+    public String govNumberCarNewClient;
+    public String manufacturerCarNewClient;
+    public String modelCarNewClient;
+    public String setDataBirthday;
+
+    public Client(){
+
+    }
+
+    public Client(String userId, String nameSurnameNewClient, int numPhoneNewClient, String govNumberCarNewClient, String manufacturerCarNewClient, String modelCarNewClient, String setDataBirthday) {
+        UserId = userId;
+        this.nameSurnameNewClient = nameSurnameNewClient;
+        this.numPhoneNewClient = numPhoneNewClient;
+        this.govNumberCarNewClient = govNumberCarNewClient;
+        this.manufacturerCarNewClient = manufacturerCarNewClient;
+        this.modelCarNewClient = modelCarNewClient;
+        this.setDataBirthday = setDataBirthday;
+    }
+
+    public String getNameSurnameNewClient() {
+        return nameSurnameNewClient;
+    }
+
+    public int getNumPhoneNewClient() {
+        return numPhoneNewClient;
+    }
+
+    public String getGovNumberCarNewClient() {
+        return govNumberCarNewClient;
+    }
+
+    public String getManufacturerCarNewClient() {
+        return manufacturerCarNewClient;
+    }
+
+    public String getModelCarNewClient() {
+        return modelCarNewClient;
+    }
+
+    public String getSetDataBirthday() {
+        return setDataBirthday;
+    }
+}
---
+++ app/src/main/java/com/example/client/Models/Order.java
@@ -0,0 +1,123 @@
+package com.example.client.Models;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Order implements Serializable {
+    public String UserId;
+    //    public String typeOfService;
+    public int startTimeHour;
+    public int startTimeMinute;
+    public int startDayOfMonth;
+    public int startTimeMonth;
+    public int startTimeYear;
+    public int endTimeHour;
+    public int endTimeMinute;
+    public int endDayOfMonth;
+    public int endTimeMonth;
+    public int endTimeYear;
+
+    public Order(){}
+
+    public Order(String userId, int startTimeHour, int startTimeMinute, int startDayOfMonth, int startTimeMonth, int startTimeYear, int endTimeHour, int endTimeMinute, int endDayOfMonth, int endTimeMonth, int endTimeYear) {
+        UserId = userId;
+        this.startTimeHour = startTimeHour;
+        this.startTimeMinute = startTimeMinute;
+        this.startDayOfMonth = startDayOfMonth;
+        this.startTimeMonth = startTimeMonth;
+        this.startTimeYear = startTimeYear;
+        this.endTimeHour = endTimeHour;
+        this.endTimeMinute = endTimeMinute;
+        this.endDayOfMonth = endDayOfMonth;
+        this.endTimeMonth = endTimeMonth;
+        this.endTimeYear = endTimeYear;
+    }
+
+    public int getStartTimeHour() {
+        return startTimeHour;
+    }
+
+    public void setStartTimeHour(int startTimeHour) {
+        this.startTimeHour = startTimeHour;
+    }
+
+    public int getStartTimeMinute() {
+        return startTimeMinute;
+    }
+
+    public void setStartTimeMinute(int startTimeMinute) {
+        this.startTimeMinute = startTimeMinute;
+    }
+
+    public int getStartDayOfMonth() {
+        return startDayOfMonth;
+    }
+
+    public void setStartDayOfMonth(int startDayOfMonth) {
+        this.startDayOfMonth = startDayOfMonth;
+    }
+
+    public int getStartTimeMonth() {
+        return startTimeMonth;
+    }
+
+    public void setStartTimeMonth(int startTimeMonth) {
+        this.startTimeMonth = startTimeMonth;
+    }
+
+    public int getStartTimeYear() {
+        return startTimeYear;
+    }
+
+    public void setStartTimeYear(int startTimeYear) {
+        this.startTimeYear = startTimeYear;
+    }
+
+    public int getEndTimeHour() {
+        return endTimeHour;
+    }
+
+    public void setEndTimeHour(int endTimeHour) {
+        this.endTimeHour = endTimeHour;
+    }
+
+    public int getEndTimeMinute() {
+        return endTimeMinute;
+    }
+
+    public void setEndTimeMinute(int endTimeMinute) {
+        this.endTimeMinute = endTimeMinute;
+    }
+
+    public int getEndDayOfMonth() {
+        return endDayOfMonth;
+    }
+
+    public void setEndDayOfMonth(int endDayOfMonth) {
+        this.endDayOfMonth = endDayOfMonth;
+    }
+
+    public int getEndTimeMonth() {
+        return endTimeMonth;
+    }
+
+    public void setEndTimeMonth(int endTimeMonth) {
+        this.endTimeMonth = endTimeMonth;
+    }
+
+    public int getEndTimeYear() {
+        return endTimeYear;
+    }
+
+    public void setEndTimeYear(int endTimeYear) {
+        this.endTimeYear = endTimeYear;
+    }
+    @Exclude
+    public Map<String, Object> toMap(){
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("startTimeHour", startTimeHour);
+
+        return result;
+    }
+}
