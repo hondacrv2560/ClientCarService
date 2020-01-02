cc981d95452ce4504deca519d9f233600993ca44
Kosogov Elias <i.kosogov@gmail.com>
2020-01-02 13:58:12 +0200

    created string connection to Fire Base created check signIn or signOut user

--- app/src/main/java/com/example/client/MainActivity.java
+++ app/src/main/java/com/example/client/MainActivity.java
@@ -1,14 +1,53 @@
 package com.example.client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    //создание подключения к БД
+    private FirebaseAuth firebaseAuth;
+    //создание подключения к авторизапции
+    private FirebaseAuth.AuthStateListener authStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//проверка на авторизацию, если клиент в приложении авторизирован
+        firebaseAuth = firebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                String TAG="test_onAuthStateChanged";
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Toast.makeText(MainActivity.this, "signed in", Toast.LENGTH_SHORT).show();
+                    MenuItem regular_customer = findViewById(R.id.regular_customer);
+                    regular_customer.setVisible(true);
+                    Log.d(TAG,"onAuthStateChanged: signed in" + user.getUid());
+
+                }else{
+                    Log.d(TAG,"onAuthStateChanged: signed out");
+                }
+            }
+        };
+        // gjkextv Uid user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user != null){
+            Toast.makeText(MainActivity.this, "signed in" + user.getUid(), Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
