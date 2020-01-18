be4443d73247c6607728c75e8d658ef8f3e4faf1
Kosogov Elias <i.kosogov@gmail.com>
2020-01-18 19:36:52 +0200

    created model Service for RecicleView Service

---
+++ app/src/main/java/com/example/client/Models/Service.java
@@ -0,0 +1,20 @@
+package com.example.client.Models;
+
+public class Service {
+    public String title_service;
+
+    public Service() {
+    }
+
+    public Service(String title_service) {
+        this.title_service = title_service;
+    }
+
+    public String getTitle_service() {
+        return title_service;
+    }
+
+    public void setTitle_service(String title_service) {
+        this.title_service = title_service;
+    }
+}
