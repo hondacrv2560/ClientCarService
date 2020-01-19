0d884c92bb095c7403c6eac758764282d39d7c3b
Kosogov Elias <i.kosogov@gmail.com>
2020-01-19 14:48:49 +0200

    created in CarWashing_3PhasesFragment override method onCreateView

--- app/src/main/java/com/example/client/Fragments/CarWashing_3PhasesFragment.java
+++ app/src/main/java/com/example/client/Fragments/CarWashing_3PhasesFragment.java
@@ -1,9 +1,17 @@
 package com.example.client.Fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.client.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,4 +26,19 @@ public class CarWashing_3PhasesFragment extends Fragment {
     DatabaseReference myDbReference = database.getReference();
     // поключение к child Orders
     DatabaseReference serviceRef = myDbReference.child("CarWashing_3Phases");
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View carWashing_3PhasesFragment = inflater.inflate(R.layout.car_washing_3phases_view, container,false);
+
+        recyclerView = carWashing_3PhasesFragment.findViewById(R.id.service_list);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        fetch();
+
+        return carWashing_3PhasesFragment;
+    }
 }
