05d262e5b51240f4e54410f4f8c4bb8ba17429fa
Kosogov Elias <i.kosogov@gmail.com>
2020-01-19 12:12:47 +0200

    added to CarWashing_3Phases fields, methods and constructors

--- app/src/main/java/com/example/client/Models/CarWashing_3Phases.java
+++ app/src/main/java/com/example/client/Models/CarWashing_3Phases.java
@@ -1,4 +1,40 @@
 package com.example.client.Models;
 
 public class CarWashing_3Phases {
+    public String idCw_3Phases;
+    public String titleCw_3Phases;
+    public String priceCw_3Phases;
+
+    public CarWashing_3Phases() {
+    }
+
+    public CarWashing_3Phases(String idCw_3Phases, String titleCw_3Phases, String priceCw_3Phases) {
+        this.idCw_3Phases = idCw_3Phases;
+        this.titleCw_3Phases = titleCw_3Phases;
+        this.priceCw_3Phases = priceCw_3Phases;
+    }
+
+    public String getIdCw_3Phases() {
+        return idCw_3Phases;
+    }
+
+    public void setIdCw_3Phases(String idCw_3Phases) {
+        this.idCw_3Phases = idCw_3Phases;
+    }
+
+    public String getTitleCw_3Phases() {
+        return titleCw_3Phases;
+    }
+
+    public void setTitleCw_3Phases(String titleCw_3Phases) {
+        this.titleCw_3Phases = titleCw_3Phases;
+    }
+
+    public String getPriceCw_3Phases() {
+        return priceCw_3Phases;
+    }
+
+    public void setPriceCw_3Phases(String priceCw_3Phases) {
+        this.priceCw_3Phases = priceCw_3Phases;
+    }
 }
