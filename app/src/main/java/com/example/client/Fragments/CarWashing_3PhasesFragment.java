6085acdebd207e99d564d1d7a7b0a2ff8edf7605
Kosogov Elias <i.kosogov@gmail.com>
2020-01-19 14:45:45 +0200

    added to CarWashing_3PhasesFragment  RecyclerView, LinearLayoutManager, FirebaseRecyclerAdapter and connecting to FireBase

--- app/src/main/java/com/example/client/Fragments/CarWashing_3PhasesFragment.java
+++ app/src/main/java/com/example/client/Fragments/CarWashing_3PhasesFragment.java
@@ -1,7 +1,21 @@
 package com.example.client.Fragments;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CarWashing_3PhasesFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    // получение ссылки на БД
+    DatabaseReference myDbReference = database.getReference();
+    // поключение к child Orders
+    DatabaseReference serviceRef = myDbReference.child("CarWashing_3Phases");
 }
