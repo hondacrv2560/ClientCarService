655922292c51b7beb2ca17044680c6c8df5bb76b
Kosogov Elias <i.kosogov@gmail.com>
2020-01-20 14:16:55 +0200

    created method onCreate in PolishingActivity and added to AndroidManifest          <activity android:name=".Activity.PolishingActivity">

--- app/src/main/java/com/example/client/Activity/PolishingActivity.java
+++ app/src/main/java/com/example/client/Activity/PolishingActivity.java
@@ -1,11 +1,14 @@
 package com.example.client.Activity;
 
+import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.client.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -21,4 +24,19 @@ public class PolishingActivity extends AppCompatActivity {
     // поключение к child Orders
     DatabaseReference serviceRef = myDbReference.child("Polishing");
     TextView txtInfo;
+    TextView txtInfo1;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.polishing_view);
+        txtInfo = findViewById(R.id.txtInfo);
+        txtInfo1 = findViewById(R.id.txtInfo1);
+
+        recyclerView = findViewById(R.id.RepairWindshield_list);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        fetch();
+    }
 }
