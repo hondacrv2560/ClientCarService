2ca14c1f5396e3b0e9a4362ef146062154a4ce1f
Kosogov Elias <i.kosogov@gmail.com>
2020-01-20 14:11:27 +0200

    created PolishingActivity and connecting to Firebase

---
+++ app/src/main/java/com/example/client/Activity/PolishingActivity.java
@@ -0,0 +1,24 @@
+package com.example.client.Activity;
+
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class PolishingActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    // получение ссылки на БД
+    DatabaseReference myDbReference = database.getReference();
+    // поключение к child Orders
+    DatabaseReference serviceRef = myDbReference.child("Polishing");
+    TextView txtInfo;
+}
