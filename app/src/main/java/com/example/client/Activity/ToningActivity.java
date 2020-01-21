e02154cd7e93fbc211927791ce6080a7e98e62cd
Kosogov Elias <i.kosogov@gmail.com>
2020-01-21 16:36:54 +0200

    created in ToningActivivty method onCreate

---
+++ app/src/main/java/com/example/client/Activity/ToningActivity.java
@@ -0,0 +1,51 @@
+package com.example.client.Activity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.client.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ToningActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    // получение ссылки на БД
+    DatabaseReference myDbReference = database.getReference();
+    // поключение к child Toning
+    DatabaseReference toningRef = myDbReference.child("Toning");
+    TextView txtInfo;
+    TextView cat1;
+    TextView cat2;
+    TextView cat3;
+    TextView cat4;
+    TextView cat5;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.toning_view);
+        txtInfo=findViewById(R.id.txtInfo);
+        cat1=findViewById(R.id.cat1);
+        cat2=findViewById(R.id.cat2);
+        cat3=findViewById(R.id.cat3);
+        cat4=findViewById(R.id.cat4);
+        cat5=findViewById(R.id.cat5);
+
+        recyclerView = findViewById(R.id.Toning_list);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        fetch();
+    }
+}
