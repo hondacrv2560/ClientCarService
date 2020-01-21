190b37e979ceed84843b239d53c27f4b7707c1d6
Kosogov Elias <i.kosogov@gmail.com>
2020-01-21 20:12:15 +0200

    created model NanoCeramics

---
+++ app/src/main/java/com/example/client/Models/NanoCeramics.java
@@ -0,0 +1,80 @@
+package com.example.client.Models;
+
+public class NanoCeramics {
+    public String idNanoCeramics;
+    public String titleNanoCeramics;
+    public String priceNanoCeramics_sedan;
+    public String priceNanoCeramics_business;
+    public String priceNanoCeramics_premium;
+    public String priceNanoCeramics_SUV;
+    public String priceNanoCeramics_BigSUV;
+
+    public NanoCeramics() {
+    }
+
+    public NanoCeramics(String idNanoCeramics, String titleNanoCeramics, String priceNanoCeramics_sedan, String priceNanoCeramics_business, String priceNanoCeramics_premium, String priceNanoCeramics_SUV, String priceNanoCeramics_BigSUV) {
+        this.idNanoCeramics = idNanoCeramics;
+        this.titleNanoCeramics = titleNanoCeramics;
+        this.priceNanoCeramics_sedan = priceNanoCeramics_sedan;
+        this.priceNanoCeramics_business = priceNanoCeramics_business;
+        this.priceNanoCeramics_premium = priceNanoCeramics_premium;
+        this.priceNanoCeramics_SUV = priceNanoCeramics_SUV;
+        this.priceNanoCeramics_BigSUV = priceNanoCeramics_BigSUV;
+    }
+
+    public String getIdNanoCeramics() {
+        return idNanoCeramics;
+    }
+
+    public void setIdNanoCeramics(String idNanoCeramics) {
+        this.idNanoCeramics = idNanoCeramics;
+    }
+
+    public String getTitleNanoCeramics() {
+        return titleNanoCeramics;
+    }
+
+    public void setTitleNanoCeramics(String titleNanoCeramics) {
+        this.titleNanoCeramics = titleNanoCeramics;
+    }
+
+    public String getPriceNanoCeramics_sedan() {
+        return priceNanoCeramics_sedan;
+    }
+
+    public void setPriceNanoCeramics_sedan(String priceNanoCeramics_sedan) {
+        this.priceNanoCeramics_sedan = priceNanoCeramics_sedan;
+    }
+
+    public String getPriceNanoCeramics_business() {
+        return priceNanoCeramics_business;
+    }
+
+    public void setPriceNanoCeramics_business(String priceNanoCeramics_business) {
+        this.priceNanoCeramics_business = priceNanoCeramics_business;
+    }
+
+    public String getPriceNanoCeramics_premium() {
+        return priceNanoCeramics_premium;
+    }
+
+    public void setPriceNanoCeramics_premium(String priceNanoCeramics_premium) {
+        this.priceNanoCeramics_premium = priceNanoCeramics_premium;
+    }
+
+    public String getPriceNanoCeramics_SUV() {
+        return priceNanoCeramics_SUV;
+    }
+
+    public void setPriceNanoCeramics_SUV(String priceNanoCeramics_SUV) {
+        this.priceNanoCeramics_SUV = priceNanoCeramics_SUV;
+    }
+
+    public String getPriceNanoCeramics_BigSUV() {
+        return priceNanoCeramics_BigSUV;
+    }
+
+    public void setPriceNanoCeramics_BigSUV(String priceNanoCeramics_BigSUV) {
+        this.priceNanoCeramics_BigSUV = priceNanoCeramics_BigSUV;
+    }
+}
