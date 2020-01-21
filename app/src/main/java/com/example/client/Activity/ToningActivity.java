0e4578180ac786584752c4aacb98e40c771f5849
Kosogov Elias <i.kosogov@gmail.com>
2020-01-21 19:01:05 +0200

    created in ToningActivivty method ViewHolder

--- app/src/main/java/com/example/client/Activity/ToningActivity.java
+++ app/src/main/java/com/example/client/Activity/ToningActivity.java
@@ -1,6 +1,8 @@
 package com.example.client.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -47,5 +49,52 @@ public class ToningActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
         fetch();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout root;
+        public TextView txtIdToning;
+        public TextView txtTitleToning;
+        public TextView txtPriceToning_sedan;
+        public TextView txtPriceToning_business;
+        public TextView txtPriceToning_premium;
+        public TextView txtPriceToning_SUV;
+        public TextView txtPriceToning_BigSUV;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            root = itemView.findViewById(R.id.list_root);
+            txtIdToning = itemView.findViewById(R.id.id_title);
+            txtTitleToning = itemView.findViewById(R.id.list_title);
+            txtPriceToning_sedan = itemView.findViewById(R.id.priceToning_sedan);
+            txtPriceToning_business = itemView.findViewById(R.id.priceToning_business);
+            txtPriceToning_premium = itemView.findViewById(R.id.priceToning_premium);
+            txtPriceToning_SUV = itemView.findViewById(R.id.priceToning_SUV);
+            txtPriceToning_BigSUV = itemView.findViewById(R.id.priceToning_BigSUV);
+
+        }
+
+        public void setTxtIdToning(String string) {
+            txtIdToning.setText(string);
+        }
+        public void setTxtTitleToning(String string) {
+            txtTitleToning.setText(string);
+        }
+        public void setTxtPriceToning_sedan(String string){
+            txtPriceToning_sedan.setText(string);
+        }
+        public void setTxtPriceToning_business(String string) {
+            txtPriceToning_business.setText(string);
+        }
+        public void setTxtPriceToning_premium(String string){
+            txtPriceToning_premium.setText(string);
+        }
+        public void setTxtPriceToning_SUV(String string) {
+            txtPriceToning_SUV.setText(string);
+        }
+        public void setTxtPriceToning_BigSUV(String string){
+            txtPriceToning_BigSUV.setText(string);
+        }
     }
 }
