92b4a03a64ca39cb3e1c7112d9b8c66c8158c78b
Kosogov Elias <i.kosogov@gmail.com>
2020-01-22 12:26:18 +0200

    created in ProtectiveFilmActivity methos onCreate

--- app/src/main/java/com/example/client/Activity/ProtectiveFilmActivity.java
+++ app/src/main/java/com/example/client/Activity/ProtectiveFilmActivity.java
@@ -1,11 +1,14 @@
 package com.example.client.Activity;
 
+import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.client.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -28,4 +31,22 @@ public class ProtectiveFilmActivity extends AppCompatActivity {
     TextView cat4;
     TextView cat5;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.protective_film_view);
+
+        txtInfo=findViewById(R.id.txtInfo);
+        cat1=findViewById(R.id.cat1);
+        cat2=findViewById(R.id.cat2);
+        cat3=findViewById(R.id.cat3);
+        cat4=findViewById(R.id.cat4);
+        cat5=findViewById(R.id.cat5);
+
+        recyclerView = findViewById(R.id.ProtectiveFilm_list);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        fetch();
+    }
 }
