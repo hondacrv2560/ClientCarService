36c1dd548a2641236cb0a711281f3a38084a596c
Kosogov Elias <i.kosogov@gmail.com>
2020-01-22 12:28:27 +0200

    created in ProtectiveFilmActivivty method ViewHolder

--- app/src/main/java/com/example/client/Activity/ProtectiveFilmActivity.java
+++ app/src/main/java/com/example/client/Activity/ProtectiveFilmActivity.java
@@ -1,6 +1,8 @@
 package com.example.client.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -48,5 +50,52 @@ public class ProtectiveFilmActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
         fetch();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout root;
+        public TextView txtIdProtectiveFilm;
+        public TextView txtTitleProtectiveFilm;
+        public TextView txtPriceProtectiveFilm_sedan;
+        public TextView txtPriceProtectiveFilm_business;
+        public TextView txtPriceProtectiveFilm_premium;
+        public TextView txtPriceProtectiveFilm_SUV;
+        public TextView txtPriceProtectiveFilm_BigSUV;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            root = itemView.findViewById(R.id.list_root);
+            txtIdProtectiveFilm = itemView.findViewById(R.id.id_title);
+            txtTitleProtectiveFilm = itemView.findViewById(R.id.list_title);
+            txtPriceProtectiveFilm_sedan = itemView.findViewById(R.id.priceProtectiveFilm_sedan);
+            txtPriceProtectiveFilm_business = itemView.findViewById(R.id.priceProtectiveFilm_business);
+            txtPriceProtectiveFilm_premium = itemView.findViewById(R.id.priceProtectiveFilm_premium);
+            txtPriceProtectiveFilm_SUV = itemView.findViewById(R.id.priceProtectiveFilm_SUV);
+            txtPriceProtectiveFilm_BigSUV = itemView.findViewById(R.id.priceProtectiveFilm_BigSUV);
+
+        }
+
+        public void setTxtIdProtectiveFilm(String string) {
+            txtIdProtectiveFilm.setText(string);
+        }
+        public void setTxtTitleProtectiveFilm(String string) {
+            txtTitleProtectiveFilm.setText(string);
+        }
+        public void setTxtPriceProtectiveFilm_sedan(String string){
+            txtPriceProtectiveFilm_sedan.setText(string);
+        }
+        public void setTxtPriceProtectiveFilm_business(String string) {
+            txtPriceProtectiveFilm_business.setText(string);
+        }
+        public void setTxtPriceProtectiveFilm_premium(String string){
+            txtPriceProtectiveFilm_premium.setText(string);
+        }
+        public void setTxtPriceProtectiveFilm_SUV(String string) {
+            txtPriceProtectiveFilm_SUV.setText(string);
+        }
+        public void setTxtPriceProtectiveFilm_BigSUV(String string){
+            txtPriceProtectiveFilm_BigSUV.setText(string);
+        }
     }
 }
