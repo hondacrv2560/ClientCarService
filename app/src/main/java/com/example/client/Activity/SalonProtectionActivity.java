1dfe1e42cf6228b8272a9621fa806e13302d2985
Kosogov Elias <i.kosogov@gmail.com>
2020-01-22 16:17:57 +0200

    created SalonProtectionActivity

---
+++ app/src/main/java/com/example/client/Activity/SalonProtectionActivity.java
@@ -0,0 +1,29 @@
+package com.example.client.Activity;
+
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SalonProtectionActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    private FirebaseRecyclerAdapter adapter;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    // получение ссылки на БД
+    DatabaseReference myDbReference = database.getReference();
+    // поключение к child SalonProtection
+    DatabaseReference serviceRef = myDbReference.child("SalonProtection");
+    TextView txtInfo;
+    TextView cat1;
+    TextView cat2;
+    TextView cat3;
+    TextView cat4;
+    TextView cat5;
+}
