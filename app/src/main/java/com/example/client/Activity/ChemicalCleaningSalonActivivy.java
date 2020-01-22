e1812a32f2640e2d30640fd10ff874499fb22c7e
Kosogov Elias <i.kosogov@gmail.com>
2020-01-22 15:41:55 +0200

    created in ChemicalCleaningSalonActivivty method onCreate

--- app/src/main/java/com/example/client/Activity/ChemicalCleaningSalonActivivy.java
+++ app/src/main/java/com/example/client/Activity/ChemicalCleaningSalonActivivy.java
@@ -1,11 +1,14 @@
 package com.example.client.Activity;
 
+import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.client.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -26,4 +29,24 @@ public class ChemicalCleaningSalonActivivy extends AppCompatActivity {
     TextView cat3;
     TextView cat4;
     TextView cat5;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chemical_cleaning_salon_view);
+        txtInfo=findViewById(R.id.txtInfo);
+        cat1=findViewById(R.id.cat1);
+        cat2=findViewById(R.id.cat2);
+        cat3=findViewById(R.id.cat3);
+        cat4=findViewById(R.id.cat4);
+        cat5=findViewById(R.id.cat5);
+
+        recyclerView = findViewById(R.id.ChemicalCleaningSalon_list);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        fetch();
+    }
+
+    
 }
