5f3ce7762acb908416aea70c80c579c488a9c980
Kosogov Elias <i.kosogov@gmail.com>
2020-01-22 12:54:22 +0200

    creating model ChemicalCleningSalon

---
+++ app/src/main/java/com/example/client/Models/ChemicalCleaningSalon.java
@@ -0,0 +1,80 @@
+package com.example.client.Models;
+
+public class ChemicalCleaningSalon {
+    public String idChemicalCleaningSalon;
+    public String titleChemicalCleaningSalon;
+    public String priceChemicalCleaningSalon_sedan;
+    public String priceChemicalCleaningSalon_business;
+    public String priceChemicalCleaningSalon_premium;
+    public String priceChemicalCleaningSalon_SUV;
+    public String priceChemicalCleaningSalon_BigSUV;
+
+    public ChemicalCleaningSalon() {
+    }
+
+    public ChemicalCleaningSalon(String idChemicalCleaningSalon, String titleChemicalCleaningSalon, String priceChemicalCleaningSalon_sedan, String priceChemicalCleaningSalon_business, String priceChemicalCleaningSalon_premium, String priceChemicalCleaningSalon_SUV, String priceChemicalCleaningSalon_BigSUV) {
+        this.idChemicalCleaningSalon = idChemicalCleaningSalon;
+        this.titleChemicalCleaningSalon = titleChemicalCleaningSalon;
+        this.priceChemicalCleaningSalon_sedan = priceChemicalCleaningSalon_sedan;
+        this.priceChemicalCleaningSalon_business = priceChemicalCleaningSalon_business;
+        this.priceChemicalCleaningSalon_premium = priceChemicalCleaningSalon_premium;
+        this.priceChemicalCleaningSalon_SUV = priceChemicalCleaningSalon_SUV;
+        this.priceChemicalCleaningSalon_BigSUV = priceChemicalCleaningSalon_BigSUV;
+    }
+
+    public String getIdChemicalCleaningSalon() {
+        return idChemicalCleaningSalon;
+    }
+
+    public void setIdChemicalCleaningSalon(String idChemicalCleaningSalon) {
+        this.idChemicalCleaningSalon = idChemicalCleaningSalon;
+    }
+
+    public String getTitleChemicalCleaningSalon() {
+        return titleChemicalCleaningSalon;
+    }
+
+    public void setTitleChemicalCleaningSalon(String titleChemicalCleaningSalon) {
+        this.titleChemicalCleaningSalon = titleChemicalCleaningSalon;
+    }
+
+    public String getPriceChemicalCleaningSalon_sedan() {
+        return priceChemicalCleaningSalon_sedan;
+    }
+
+    public void setPriceChemicalCleaningSalon_sedan(String priceChemicalCleaningSalon_sedan) {
+        this.priceChemicalCleaningSalon_sedan = priceChemicalCleaningSalon_sedan;
+    }
+
+    public String getPriceChemicalCleaningSalon_business() {
+        return priceChemicalCleaningSalon_business;
+    }
+
+    public void setPriceChemicalCleaningSalon_business(String priceChemicalCleaningSalon_business) {
+        this.priceChemicalCleaningSalon_business = priceChemicalCleaningSalon_business;
+    }
+
+    public String getPriceChemicalCleaningSalon_premium() {
+        return priceChemicalCleaningSalon_premium;
+    }
+
+    public void setPriceChemicalCleaningSalon_premium(String priceChemicalCleaningSalon_premium) {
+        this.priceChemicalCleaningSalon_premium = priceChemicalCleaningSalon_premium;
+    }
+
+    public String getPriceChemicalCleaningSalon_SUV() {
+        return priceChemicalCleaningSalon_SUV;
+    }
+
+    public void setPriceChemicalCleaningSalon_SUV(String priceChemicalCleaningSalon_SUV) {
+        this.priceChemicalCleaningSalon_SUV = priceChemicalCleaningSalon_SUV;
+    }
+
+    public String getPriceChemicalCleaningSalon_BigSUV() {
+        return priceChemicalCleaningSalon_BigSUV;
+    }
+
+    public void setPriceChemicalCleaningSalon_BigSUV(String priceChemicalCleaningSalon_BigSUV) {
+        this.priceChemicalCleaningSalon_BigSUV = priceChemicalCleaningSalon_BigSUV;
+    }
+}
