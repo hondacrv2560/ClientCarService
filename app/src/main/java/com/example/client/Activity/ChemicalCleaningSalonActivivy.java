e9dfe2d58b8d40ad903876bd1fb8c6b3d35a0150
Kosogov Elias <i.kosogov@gmail.com>
2020-01-22 15:44:15 +0200

    created in ChemicalCleaningSalonActivivty method ViewHolder

--- app/src/main/java/com/example/client/Activity/ChemicalCleaningSalonActivivy.java
+++ app/src/main/java/com/example/client/Activity/ChemicalCleaningSalonActivivy.java
@@ -1,6 +1,8 @@
 package com.example.client.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -48,5 +50,50 @@ public class ChemicalCleaningSalonActivivy extends AppCompatActivity {
         fetch();
     }
 
-    
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout root;
+        public TextView txtIdChemicalCleaningSalon;
+        public TextView txtTitleChemicalCleaningSalon;
+        public TextView txtPriceChemicalCleaningSalon_sedan;
+        public TextView txtPriceChemicalCleaningSalon_business;
+        public TextView txtPriceChemicalCleaningSalon_premium;
+        public TextView txtPriceChemicalCleaningSalon_SUV;
+        public TextView txtPriceChemicalCleaningSalon_BigSUV;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            root = itemView.findViewById(R.id.list_root);
+            txtIdChemicalCleaningSalon = itemView.findViewById(R.id.id_title);
+            txtTitleChemicalCleaningSalon = itemView.findViewById(R.id.list_title);
+            txtPriceChemicalCleaningSalon_sedan = itemView.findViewById(R.id.priceChemicalCleaningSalon_sedan);
+            txtPriceChemicalCleaningSalon_business = itemView.findViewById(R.id.priceChemicalCleaningSalon_business);
+            txtPriceChemicalCleaningSalon_premium = itemView.findViewById(R.id.priceChemicalCleaningSalon_premium);
+            txtPriceChemicalCleaningSalon_SUV = itemView.findViewById(R.id.priceChemicalCleaningSalon_SUV);
+            txtPriceChemicalCleaningSalon_BigSUV = itemView.findViewById(R.id.priceChemicalCleaningSalon_BigSUV);
+
+        }
+
+        public void setTxtIdChemicalCleaningSalon(String string) {
+            txtIdChemicalCleaningSalon.setText(string);
+        }
+        public void setTxtTitleChemicalCleaningSalon(String string) {
+            txtTitleChemicalCleaningSalon.setText(string);
+        }
+        public void setTxtPriceChemicalCleaningSalon_sedan(String string){
+            txtPriceChemicalCleaningSalon_sedan.setText(string);
+        }
+        public void setTxtPriceChemicalCleaningSalon_business(String string) {
+            txtPriceChemicalCleaningSalon_business.setText(string);
+        }
+        public void setTxtPriceChemicalCleaningSalon_premium(String string){
+            txtPriceChemicalCleaningSalon_premium.setText(string);
+        }
+        public void setTxtPriceChemicalCleaningSalon_SUV(String string) {
+            txtPriceChemicalCleaningSalon_SUV.setText(string);
+        }
+        public void setTxtPriceChemicalCleaningSalon_BigSUV(String string){
+            txtPriceChemicalCleaningSalon_BigSUV.setText(string);
+        }
+    }
 }
