d1b88568ab73b7627a4d07d773be6a35109da9ce
Kosogov Elias <i.kosogov@gmail.com>
2020-01-25 15:04:21 +0200

    added to ServiceClient field public String imagePath

--- app/src/main/java/com/example/client/Models/ServiceClient.java
+++ app/src/main/java/com/example/client/Models/ServiceClient.java
@@ -3,13 +3,14 @@ package com.example.client.Models;
 public class ServiceClient {
     public String idService;
     public String title_service;
-
+    public String imagePath;
     public ServiceClient() {
     }
 
-    public ServiceClient(String idService, String title_service) {
+    public ServiceClient(String idService, String title_service, String imagePath) {
         this.idService = idService;
         this.title_service = title_service;
+        this.imagePath = imagePath;
     }
 
     public String getIdService() {
@@ -26,5 +27,13 @@ public class ServiceClient {
 
     public void setTitle_service(String title_service) {
         this.title_service = title_service;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
     }
 }
