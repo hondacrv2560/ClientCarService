9c6e00f865e2cc95dac1eb52dc33b24ede3f2567
Kosogov Elias <i.kosogov@gmail.com>
2020-01-28 15:04:09 +0200

    changed in ToDayActivity color download from DB

--- app/src/main/java/com/example/client/Classes/ToDayActivity.java
+++ app/src/main/java/com/example/client/Classes/ToDayActivity.java
@@ -1,5 +1,6 @@
 package com.example.client.Classes;
 
+import android.graphics.Color;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -39,8 +40,8 @@ public class ToDayActivity extends ToDay {
             endTime.set(Calendar.DAY_OF_MONTH, Integer.parseInt(snapshots.get(i).child("endDayOfMonth").getValue().toString()));
             endTime.set(Calendar.HOUR_OF_DAY, Integer.parseInt(snapshots.get(i).child("endTimeHour").getValue().toString()));
             WeekViewEvent event = new WeekViewEvent(snapshots.get(i).getKey().hashCode(), getEventTitle(startTime), startTime, endTime);
-            event.setColor(getResources().getColor(R.color.event_color_03));
-
+//            event.setColor(getResources().getColor(R.color.event_color_03));
+            event.setColor(Color.parseColor(snapshots.get(i).child("color").getValue().toString()));
 
             result.add(event);
         }
