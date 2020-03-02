947f08ae2646790ac142b633c29d4e59ec951cfa
Kosogov Elias <i.kosogov@gmail.com>
2020-03-02 17:11:53 +0200

    created frament QrREaderFragment

--- app/src/main/java/com/example/client/Activity/ActivityClientInfo.java
+++ app/src/main/java/com/example/client/Activity/ActivityClientInfo.java
@@ -1,52 +1,35 @@
 package com.example.client.Activity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.client.Classes.ClientInfoAdapter;
-import com.example.client.Models.Client;
+import com.example.client.Fragments.QrReaderFragment;
 import com.example.client.Models.ClientInfo;
 import com.example.client.R;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.LuminanceSource;
-import com.google.zxing.MultiFormatReader;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.PlanarYUVLuminanceSource;
-import com.google.zxing.Result;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.common.HybridBinarizer;
-import com.journeyapps.barcodescanner.BarcodeEncoder;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 public class ActivityClientInfo extends AppCompatActivity {
-
+    FragmentManager fragmentManager;
+    FragmentTransaction transaction;
     private RecyclerView recyclerView;
     private ClientInfoAdapter clientInfoAdapter;
     private ArrayList<ClientInfo> clientInfoList;
@@ -56,12 +39,8 @@ public class ActivityClientInfo extends AppCompatActivity {
     Button btn_findNameSurname;
     Button btn_findGovNumber;
     Button btn_findQrCode;
-
-
     DatabaseReference dbInfoClient;
-
     Button addCar;
-    ImageView qrCode;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -75,7 +54,6 @@ public class ActivityClientInfo extends AppCompatActivity {
         btn_findGovNumber = findViewById(R.id.findGovNumber);
         btn_findQrCode = findViewById(R.id.findQrCode);
         addCar = findViewById(R.id.addCar);
-        qrCode = findViewById(R.id.qrCodeView);
         recyclerView = findViewById(R.id.clientInfo);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -104,6 +82,16 @@ public class ActivityClientInfo extends AppCompatActivity {
                 query.addListenerForSingleValueEvent(valueEventListener);
             }
         });
+        find_Client_Qr_Code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager=getSupportFragmentManager();
+                QrReaderFragment qrReaderFragment=new QrReaderFragment();
+                transaction=fragmentManager.beginTransaction();
+                transaction.replace(R.id.frClientInfoView,qrReaderFragment);
+                transaction.commit();
+            }
+        });
 
         addCar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,6 +111,7 @@ public class ActivityClientInfo extends AppCompatActivity {
 //        } catch (WriterException e) {
 //            e.printStackTrace();
 //        }
+
     }
 
     ValueEventListener valueEventListener = new ValueEventListener() {
