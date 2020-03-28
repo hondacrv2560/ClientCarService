efa1caefe7bc5ef97888c47d81e790e7026e2832
Kosogov Elias <i.kosogov@gmail.com>
2020-03-28 17:24:46 +0200

    created model CarWashing_Standart

---
+++ app/src/main/java/com/example/client/Models/CarWashing_Standart.java
@@ -0,0 +1,80 @@
+package com.example.client.Models;
+
+public class CarWashing_Standart {
+    public String idCw_Standart;
+    public String titleCw_Standart;
+    public String priceCw_Standart_sedan;
+    public String priceCw_Standart_business;
+    public String priceCw_Standart_premium;
+    public String priceCw_Standart_SUV;
+    public String priceCw_Standart_BigSUV;
+
+    public CarWashing_Standart() {
+    }
+
+    public CarWashing_Standart(String idCw_Standart, String titleCw_Standart, String priceCw_Standart_sedan, String priceCw_Standart_business, String priceCw_Standart_premium, String priceCw_Standart_SUV, String priceCw_Standart_BigSUV) {
+        this.idCw_Standart = idCw_Standart;
+        this.titleCw_Standart = titleCw_Standart;
+        this.priceCw_Standart_sedan = priceCw_Standart_sedan;
+        this.priceCw_Standart_business = priceCw_Standart_business;
+        this.priceCw_Standart_premium = priceCw_Standart_premium;
+        this.priceCw_Standart_SUV = priceCw_Standart_SUV;
+        this.priceCw_Standart_BigSUV = priceCw_Standart_BigSUV;
+    }
+
+    public String getIdCw_Standart() {
+        return idCw_Standart;
+    }
+
+    public void setIdCw_Standart(String idCw_Standart) {
+        this.idCw_Standart = idCw_Standart;
+    }
+
+    public String getTitleCw_Standart() {
+        return titleCw_Standart;
+    }
+
+    public void setTitleCw_Standart(String titleCw_Standart) {
+        this.titleCw_Standart = titleCw_Standart;
+    }
+
+    public String getPriceCw_Standart_sedan() {
+        return priceCw_Standart_sedan;
+    }
+
+    public void setPriceCw_Standart_sedan(String priceCw_Standart_sedan) {
+        this.priceCw_Standart_sedan = priceCw_Standart_sedan;
+    }
+
+    public String getPriceCw_Standart_business() {
+        return priceCw_Standart_business;
+    }
+
+    public void setPriceCw_Standart_business(String priceCw_Standart_business) {
+        this.priceCw_Standart_business = priceCw_Standart_business;
+    }
+
+    public String getPriceCw_Standart_premium() {
+        return priceCw_Standart_premium;
+    }
+
+    public void setPriceCw_Standart_premium(String priceCw_Standart_premium) {
+        this.priceCw_Standart_premium = priceCw_Standart_premium;
+    }
+
+    public String getPriceCw_Standart_SUV() {
+        return priceCw_Standart_SUV;
+    }
+
+    public void setPriceCw_Standart_SUV(String priceCw_Standart_SUV) {
+        this.priceCw_Standart_SUV = priceCw_Standart_SUV;
+    }
+
+    public String getPriceCw_Standart_BigSUV() {
+        return priceCw_Standart_BigSUV;
+    }
+
+    public void setPriceCw_Standart_BigSUV(String priceCw_Standart_BigSUV) {
+        this.priceCw_Standart_BigSUV = priceCw_Standart_BigSUV;
+    }
+}
