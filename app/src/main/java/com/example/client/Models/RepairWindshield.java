34fe94c36f2b1c112cba901a70882128dd0f4645
Kosogov Elias <i.kosogov@gmail.com>
2020-03-28 15:38:51 +0200

    added fields to model RepairWindshield

--- app/src/main/java/com/example/client/Models/RepairWindshield.java
+++ app/src/main/java/com/example/client/Models/RepairWindshield.java
@@ -3,15 +3,23 @@ package com.example.client.Models;
 public class RepairWindshield {
     public String idRepairWindshield;
     public String titleRepairWindshield;
-    public String priceRepairWindshield;
+    public String priceRepairWindshield_sedan;
+    public String priceRepairWindshield_business;
+    public String priceRepairWindshield_premium;
+    public String priceRepairWindshield_SUV;
+    public String priceRepairWindshield_BigSUV;
 
     public RepairWindshield() {
     }
 
-    public RepairWindshield(String idRepairWindshield, String titleRepairWindshield, String priceRepairWindshield) {
+    public RepairWindshield(String idRepairWindshield, String titleRepairWindshield, String priceRepairWindshield_sedan, String priceRepairWindshield_business, String priceRepairWindshield_premium, String priceRepairWindshield_SUV, String priceRepairWindshield_BigSUV) {
         this.idRepairWindshield = idRepairWindshield;
         this.titleRepairWindshield = titleRepairWindshield;
-        this.priceRepairWindshield = priceRepairWindshield;
+        this.priceRepairWindshield_sedan = priceRepairWindshield_sedan;
+        this.priceRepairWindshield_business = priceRepairWindshield_business;
+        this.priceRepairWindshield_premium = priceRepairWindshield_premium;
+        this.priceRepairWindshield_SUV = priceRepairWindshield_SUV;
+        this.priceRepairWindshield_BigSUV = priceRepairWindshield_BigSUV;
     }
 
     public String getIdRepairWindshield() {
@@ -30,11 +38,43 @@ public class RepairWindshield {
         this.titleRepairWindshield = titleRepairWindshield;
     }
 
-    public String getPriceRepairWindshield() {
-        return priceRepairWindshield;
+    public String getPriceRepairWindshield_sedan() {
+        return priceRepairWindshield_sedan;
     }
 
-    public void setPriceRepairWindshield(String priceRepairWindshield) {
-        this.priceRepairWindshield = priceRepairWindshield;
+    public void setPriceRepairWindshield_sedan(String priceRepairWindshield_sedan) {
+        this.priceRepairWindshield_sedan = priceRepairWindshield_sedan;
+    }
+
+    public String getPriceRepairWindshield_business() {
+        return priceRepairWindshield_business;
+    }
+
+    public void setPriceRepairWindshield_business(String priceRepairWindshield_business) {
+        this.priceRepairWindshield_business = priceRepairWindshield_business;
+    }
+
+    public String getPriceRepairWindshield_premium() {
+        return priceRepairWindshield_premium;
+    }
+
+    public void setPriceRepairWindshield_premium(String priceRepairWindshield_premium) {
+        this.priceRepairWindshield_premium = priceRepairWindshield_premium;
+    }
+
+    public String getPriceRepairWindshield_SUV() {
+        return priceRepairWindshield_SUV;
+    }
+
+    public void setPriceRepairWindshield_SUV(String priceRepairWindshield_SUV) {
+        this.priceRepairWindshield_SUV = priceRepairWindshield_SUV;
+    }
+
+    public String getPriceRepairWindshield_BigSUV() {
+        return priceRepairWindshield_BigSUV;
+    }
+
+    public void setPriceRepairWindshield_BigSUV(String priceRepairWindshield_BigSUV) {
+        this.priceRepairWindshield_BigSUV = priceRepairWindshield_BigSUV;
     }
 }
