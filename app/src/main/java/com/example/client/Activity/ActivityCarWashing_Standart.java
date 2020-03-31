752d39b74b486a6f9c097f9576b03e3564716bac
Kosogov Elias <i.kosogov@gmail.com>
2020-03-31 15:46:14 +0300

    created method fetch() in activity ActivityCarWashing_Standart

--- app/src/main/java/com/example/client/Activity/ActivityCarWashing_Standart.java
+++ app/src/main/java/com/example/client/Activity/ActivityCarWashing_Standart.java
@@ -1,17 +1,28 @@
 package com.example.client.Activity;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.client.Models.CarWashing_Standart;
 import com.example.client.R;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class ActivityCarWashing_Standart extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -24,9 +35,10 @@ public class ActivityCarWashing_Standart extends AppCompatActivity {
     // поключение к child CarWashing
     DatabaseReference carWashingRef = myDbReference.child("CarWashing");
 
+
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.tire_fitting_view);
 
         recyclerView = findViewById(R.id.CarWashing_Standart_list);
@@ -88,5 +100,63 @@ public class ActivityCarWashing_Standart extends AppCompatActivity {
         }
     }
 
+    private void fetch() {
+        Query query = carWashingRef;
+
+        FirebaseRecyclerOptions<CarWashing_Standart> options =
+                new FirebaseRecyclerOptions.Builder<CarWashing_Standart>()
+                        .setQuery(query, new SnapshotParser<CarWashing_Standart>() {
+                            @NonNull
+                            @Override
+                            public CarWashing_Standart parseSnapshot(@NonNull DataSnapshot snapshot) {
+                                return new CarWashing_Standart(snapshot.child("idCW_Standart").getValue().toString(),
+                                        snapshot.child("titleCW_Standart").getValue().toString(),
+                                        snapshot.child("priceCW_Standart_sedan").getValue().toString(),
+                                        snapshot.child("priceCW_Standart_business").getValue().toString(),
+                                        snapshot.child("priceCW_Standart_premium").getValue().toString(),
+                                        snapshot.child("priceCW_Standart_SUV").getValue().toString(),
+                                        snapshot.child("priceCW_Standart_BigSUV").getValue().toString());
+                            }
+                        })
+                        .build();
+
+        adapter = new FirebaseRecyclerAdapter<CarWashing_Standart, ActivityCarWashing_Standart.ViewHolder>(options) {
+
+            @Override
+            public ActivityCarWashing_Standart.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.car_washing_standart_list, parent, false);
+
+                return new ActivityCarWashing_Standart.ViewHolder(view);
+            }
+
+
+            @Override
+            protected void onBindViewHolder(ActivityCarWashing_Standart.ViewHolder holder, final int position, CarWashing_Standart carWashing_standart) {
+                holder.setTxtIdCarWashingStandart(carWashing_standart.getIdCw_Standart());
+                holder.setTxtTitleCarWashingStandart(carWashing_standart.getTitleCw_Standart());
+                holder.setTxtCarWashingStandart_sedan(carWashing_standart.getPriceCw_Standart_sedan());
+                holder.setTxtPriceCarWashingStandart_business(carWashing_standart.getPriceCw_Standart_business());
+                holder.setTxtPriceCarWashingStandart_premium(carWashing_standart.getPriceCw_Standart_premium());
+                holder.setTxtPriceCarWashingStandart_SUV(carWashing_standart.getPriceCw_Standart_SUV());
+                holder.setTxtPriceCarWashingStandart_BigSUV(carWashing_standart.getPriceCw_Standart_BigSUV());
+
+                holder.root.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(ActivityCarWashing_Standart.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
 
 }
