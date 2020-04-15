72bbc0aa09a55ef143827796f81c3251aa3c9a77
Kosogov Elias <i.kosogov@gmail.com>
2020-04-15 13:05:03 +0300

    added to activity ActivityFullOrderCW_Standart items spinner to methos setOnItemSelectedListener()

--- app/src/main/java/com/example/client/Activity/ActivityFullOrderCW_Standart.java
+++ app/src/main/java/com/example/client/Activity/ActivityFullOrderCW_Standart.java
@@ -134,6 +134,34 @@ public class ActivityFullOrderCW_Standart extends AppCompatActivity {
                             Intent repairWindshield = new Intent(ActivityFullOrderCW_Standart.this, ActivityFullOrder_RepairWindshield.class);
                             startActivity(repairWindshield);
                             break;
+                        case 3:
+                            Intent polishing = new Intent(ActivityFullOrderCW_Standart.this, ActivityFullOrder_Polishing.class);
+                            startActivity(polishing);
+                            break;
+                        case 4:
+                            Intent nanoCeramics = new Intent(ActivityFullOrderCW_Standart.this, ActivityFullOrder_Nanoceramics.class);
+                            startActivity(nanoCeramics);
+                            break;
+                        case 5:
+                            Intent protectiveFilm = new Intent(ActivityFullOrderCW_Standart.this, ActivityFullOrder_ProtectiveFilm.class);
+                            startActivity(protectiveFilm);
+                            break;
+                        case 6:
+                            Intent chemicalCleaning = new Intent(ActivityFullOrderCW_Standart.this, ActivityFullOrder_ChemicalCleaningSalon.class);
+                            startActivity(chemicalCleaning);
+                            break;
+                        case 7:
+                            Intent protectiveSalon = new Intent(ActivityFullOrderCW_Standart.this, ActivityFullOrder_SalonProtection.class);
+                            startActivity(protectiveSalon);
+                            break;
+                        case 8:
+                            Intent tireFitting = new Intent(ActivityFullOrderCW_Standart.this, ActivityFullOrder_TireFitting.class);
+                            startActivity(tireFitting);
+                            break;
+                        case 9:
+                            Intent toning = new Intent(ActivityFullOrderCW_Standart.this, ActivityFullOrder_Toning.class);
+                            startActivity(toning);
+                            break;
                     }
                 }
             }
