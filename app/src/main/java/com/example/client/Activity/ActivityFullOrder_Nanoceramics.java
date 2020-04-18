e472c5841cc1b6f3ab4412a6798cc2165ff610ee
Elias Kosogov <i.kosogov@gmail.com>
2020-04-18 07:49:05 +0300

    added to activity ActivityFullOrder_Nanoceramics  items spinner to methos setOnItemSelectedListener()

--- app/src/main/java/com/example/client/Activity/ActivityFullOrder_Nanoceramics.java
+++ app/src/main/java/com/example/client/Activity/ActivityFullOrder_Nanoceramics.java
@@ -133,12 +133,35 @@ public class ActivityFullOrder_Nanoceramics extends AppCompatActivity {
                             startActivity(cwStandart);
                             break;
                         case 2:
-                            Intent repairWindshield = new Intent(ActivityFullOrder_Nanoceramics.this, ActivityFullOrder_Polishing.class);
+                            Intent repairWindshield = new Intent(ActivityFullOrder_Nanoceramics.this, ActivityFullOrder_RepairWindshield.class);
                             startActivity(repairWindshield);
                             break;
                         case 3:
+                            Intent polishing = new Intent(ActivityFullOrder_Nanoceramics.this, ActivityFullOrder_Polishing.class);
+                            startActivity(polishing);
                             break;
-
+                        case 4:
+                            break;
+                        case 5:
+                            Intent protectiveFilm = new Intent(ActivityFullOrder_Nanoceramics.this, ActivityFullOrder_ProtectiveFilm.class);
+                            startActivity(protectiveFilm);
+                            break;
+                        case 6:
+                            Intent chemicalCleaning = new Intent(ActivityFullOrder_Nanoceramics.this, ActivityFullOrder_ChemicalCleaningSalon.class);
+                            startActivity(chemicalCleaning);
+                            break;
+                        case 7:
+                            Intent protectiveSalon = new Intent(ActivityFullOrder_Nanoceramics.this, ActivityFullOrder_SalonProtection.class);
+                            startActivity(protectiveSalon);
+                            break;
+                        case 8:
+                            Intent tireFitting = new Intent(ActivityFullOrder_Nanoceramics.this, ActivityFullOrder_TireFitting.class);
+                            startActivity(tireFitting);
+                            break;
+                        case 9:
+                            Intent toning = new Intent(ActivityFullOrder_Nanoceramics.this, ActivityFullOrder_Toning.class);
+                            startActivity(toning);
+                            break;
                     }
                 }
             }
