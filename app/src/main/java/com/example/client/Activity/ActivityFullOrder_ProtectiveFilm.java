67728b760934a481fdbe501bffa93db413ae8326
Elias Kosogov <i.kosogov@gmail.com>
2020-04-18 07:52:03 +0300

    added to activity ActivityFullOrder_ProtectiveFilm   items spinner to methos setOnItemSelectedListener()

--- app/src/main/java/com/example/client/Activity/ActivityFullOrder_ProtectiveFilm.java
+++ app/src/main/java/com/example/client/Activity/ActivityFullOrder_ProtectiveFilm.java
@@ -133,12 +133,35 @@ public class ActivityFullOrder_ProtectiveFilm extends AppCompatActivity {
                             startActivity(cwStandart);
                             break;
                         case 2:
-                            Intent repairWindshield = new Intent(ActivityFullOrder_ProtectiveFilm.this, ActivityFullOrder_Polishing.class);
+                            Intent repairWindshield = new Intent(ActivityFullOrder_ProtectiveFilm.this, ActivityFullOrder_RepairWindshield.class);
                             startActivity(repairWindshield);
                             break;
                         case 3:
+                            Intent polishing = new Intent(ActivityFullOrder_ProtectiveFilm.this, ActivityFullOrder_Polishing.class);
+                            startActivity(polishing);
                             break;
-
+                        case 4:
+                            Intent nanoCeramics = new Intent(ActivityFullOrder_ProtectiveFilm.this, ActivityFullOrder_Nanoceramics.class);
+                            startActivity(nanoCeramics);
+                            break;
+                        case 5:
+                            break;
+                        case 6:
+                            Intent chemicalCleaning = new Intent(ActivityFullOrder_ProtectiveFilm.this, ActivityFullOrder_ChemicalCleaningSalon.class);
+                            startActivity(chemicalCleaning);
+                            break;
+                        case 7:
+                            Intent protectiveSalon = new Intent(ActivityFullOrder_ProtectiveFilm.this, ActivityFullOrder_SalonProtection.class);
+                            startActivity(protectiveSalon);
+                            break;
+                        case 8:
+                            Intent tireFitting = new Intent(ActivityFullOrder_ProtectiveFilm.this, ActivityFullOrder_TireFitting.class);
+                            startActivity(tireFitting);
+                            break;
+                        case 9:
+                            Intent toning = new Intent(ActivityFullOrder_ProtectiveFilm.this, ActivityFullOrder_Toning.class);
+                            startActivity(toning);
+                            break;
                     }
                 }
             }
