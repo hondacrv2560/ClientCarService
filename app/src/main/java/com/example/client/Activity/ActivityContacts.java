4014686d10a29d2b1d96af5bd351d928371f30ff
Kosogov Elias <i.kosogov@gmail.com>
2020-05-30 18:52:07 +0300

    click handlers added to ActivityContacts to go to social networks

--- app/src/main/java/com/example/client/Activity/ActivityContacts.java
+++ app/src/main/java/com/example/client/Activity/ActivityContacts.java
@@ -1,10 +1,13 @@
 package com.example.client.Activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,20 +22,34 @@ import com.example.client.R;
 
 public class ActivityContacts extends AppCompatActivity {
 
+    Context context;
     TextView kyivstar;
     TextView vodafone;
     TextView lifecel;
     TextView intertelecom;
+    ImageView imgFacebook;
+    ImageView imgViber;
+    ImageView imgWhatsApp;
+    ImageView imgTelegram;
+    ImageView imgYouTube;
+    public static String FACEBOOK_URL = "https://www.facebook.com/100052101661602";
+    public static String FACEBOOK_PAGE_ID = "100052101661602";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
+        context=getApplicationContext();
         TextView adress = findViewById(R.id.adress);
         kyivstar = findViewById(R.id.kyivstar);
         vodafone = findViewById(R.id.vodafone);
         lifecel = findViewById(R.id.lifecel);
         intertelecom = findViewById(R.id.intertelecom);
+        imgFacebook = findViewById(R.id.facebook);
+        imgViber = findViewById(R.id.viber);
+        imgWhatsApp = findViewById(R.id.whatsapp);
+        imgTelegram = findViewById(R.id.telegram);
+        imgYouTube = findViewById(R.id.youtube);
 
         ActionBar actionBar=getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
@@ -43,6 +60,87 @@ public class ActivityContacts extends AppCompatActivity {
             public void onClick(View v) {
                 Intent showMap = new Intent(ActivityContacts.this, MapsActivity.class);
                 startActivity(showMap);
+            }
+        });
+
+        imgViber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("viber://invite.viber.com/?g2=AQAEKUdVIalZD0uWmh5cIynWi2ncuVluAcrbD1nOhfNiw9eLL9OawigelleHfi6U"));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(ActivityContacts.this, "Нужное приложение не найдено", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        imgFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isAppInstalled()){
+                    Intent facebookIntent = new Intent(Intent.ACTION_VIEW);
+                    String facebookUrl = getFacebookPageURL(context);
+                    facebookIntent.setData(Uri.parse(facebookUrl));
+                    startActivity(facebookIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Нужное приложение не найдено", Toast.LENGTH_SHORT).show();
+                }
+//                try{
+//                    Intent intentFacebook = new Intent(Intent.ACTION_VIEW);
+//                    intentFacebook.setData(Uri.parse("fb://page/100052101661602"));
+//                    startActivity(intentFacebook);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                    Toast.makeText(ActivityContacts.this, "Нужное приложение не найдено", Toast.LENGTH_SHORT).show();
+//                }
+
+            }
+        });
+        imgTelegram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intentTelegram = new Intent(Intent.ACTION_VIEW);
+                    intentTelegram.setPackage("org.telegram.messenger");
+                    intentTelegram.setData(Uri.parse("https://t.me/akva_deteiling"));
+                    startActivity(intentTelegram);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(ActivityContacts.this, "Нужное приложение не найдено", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        imgWhatsApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intentWhatsApp = new Intent(Intent.ACTION_VIEW);
+                    intentWhatsApp.setPackage("com.whatsapp");
+                    intentWhatsApp.setData(Uri.parse("https://chat.whatsapp.com/FZqkpMRFfQS643peyZvOsN"));
+                    startActivity(intentWhatsApp);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(ActivityContacts.this, "Нужное приложение не найдено", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        imgYouTube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("google.com"));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(ActivityContacts.this, "Нужное приложение не найдено", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
@@ -85,6 +183,27 @@ public class ActivityContacts extends AppCompatActivity {
         }
     }
 
+    public String getFacebookPageURL(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            int versionCode = packageManager.getPackageInfo("com.facebook.orca", 0).versionCode;
+            if (versionCode >= 3002850) { //newer versions of fb app
+                return "fb://facewebmodal/f?href=" + FACEBOOK_URL;
+            } else { //older versions of fb app
+                return "fb://page/" + FACEBOOK_PAGE_ID;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            return FACEBOOK_URL; //normal web url
+        }
+    }
 
+    public boolean isAppInstalled() {
+        try {
+            getApplicationContext().getPackageManager().getApplicationInfo("com.facebook.katana", 0);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
 
 }
