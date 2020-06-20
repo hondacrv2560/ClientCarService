902da62405dda58cda9b7a18f1d5d5a6af14c975
Kosogov Elias <i.kosogov@gmail.com>
2020-06-20 16:07:21 +0300

    added method setUpperCase() to ActivivtyAddStaff

--- app/src/main/java/com/example/client/Activity/ActivivtyAddStaff.java
+++ app/src/main/java/com/example/client/Activity/ActivivtyAddStaff.java
@@ -2,6 +2,8 @@ package com.example.client.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,21 +38,27 @@ public class ActivivtyAddStaff extends AppCompatActivity {
     TextView dateBirthStaff;
     TextView addStaff;
 
+    String strFunctionStaff;
+    String strNameStaff;
+    String strSurnameStaff;
+    String strSeriesPassportStaff;
+    String strPassportIssuingAuthorityStaff;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activivty_add_staff);
 
         functionStaff = findViewById(R.id.functionStaff);
-        nameStaff  = findViewById(R.id.nameStaff);
-        surnameStaff  = findViewById(R.id.surnameStaff);
-        numphoneStaff  = findViewById(R.id.numphoneStaff);
-        seriesPassportStaff  = findViewById(R.id.seriesPassportStaff);
-        numberPassportStaff  = findViewById(R.id.numberPassportStaff);
-        passportIssuingAuthorityStaff  = findViewById(R.id.passportIssuingAuthorityStaff);
-        dateIssuePassportStaff  = findViewById(R.id.dateIssuePassportStaff);
-        dateBirthStaff  = findViewById(R.id.dateBirthStaff);
-        addStaff  = findViewById(R.id.addStaff);
+        nameStaff = findViewById(R.id.nameStaff);
+        surnameStaff = findViewById(R.id.surnameStaff);
+        numphoneStaff = findViewById(R.id.numphoneStaff);
+        seriesPassportStaff = findViewById(R.id.seriesPassportStaff);
+        numberPassportStaff = findViewById(R.id.numberPassportStaff);
+        passportIssuingAuthorityStaff = findViewById(R.id.passportIssuingAuthorityStaff);
+        dateIssuePassportStaff = findViewById(R.id.dateIssuePassportStaff);
+        dateBirthStaff = findViewById(R.id.dateBirthStaff);
+        addStaff = findViewById(R.id.addStaff);
 
         addStaff.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,6 +80,108 @@ public class ActivivtyAddStaff extends AppCompatActivity {
                 Toast.makeText(ActivivtyAddStaff.this, key, Toast.LENGTH_SHORT).show();
             }
         });
+        setUpperCase();
     }
 
+    public void setUpperCase() {
+        functionStaff.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                strFunctionStaff = s.toString();
+                if (!strFunctionStaff.equals(strFunctionStaff.toUpperCase())) {
+                    strFunctionStaff = strFunctionStaff.toUpperCase();
+                    functionStaff.setText(strFunctionStaff);
+                }
+                functionStaff.setSelection(functionStaff.getText().length());
+            }
+        });
+
+        nameStaff.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                strNameStaff = s.toString();
+                if (!strNameStaff.equals(strNameStaff.toUpperCase())) {
+                    strNameStaff = strNameStaff.toUpperCase();
+                    nameStaff.setText(strNameStaff);
+                }
+                nameStaff.setSelection(nameStaff.getText().length());
+            }
+        });
+
+        surnameStaff.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                strSurnameStaff = s.toString();
+                if (!strSurnameStaff.equals(strSurnameStaff.toUpperCase())) {
+                    strSurnameStaff = strSurnameStaff.toUpperCase();
+                    surnameStaff.setText(strSurnameStaff);
+                }
+                surnameStaff.setSelection(surnameStaff.getText().length());
+            }
+        });
+
+        seriesPassportStaff.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                strSeriesPassportStaff = s.toString();
+                if (!strSeriesPassportStaff.equals(strSeriesPassportStaff.toUpperCase())) {
+                    strSeriesPassportStaff = strSeriesPassportStaff.toUpperCase();
+                    seriesPassportStaff.setText(strSeriesPassportStaff);
+                }
+                seriesPassportStaff.setSelection(seriesPassportStaff.getText().length());
+            }
+        });
+
+        passportIssuingAuthorityStaff.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                strPassportIssuingAuthorityStaff = s.toString();
+                if (!strPassportIssuingAuthorityStaff.equals(strPassportIssuingAuthorityStaff.toUpperCase())) {
+                    strPassportIssuingAuthorityStaff = strPassportIssuingAuthorityStaff.toUpperCase();
+                    passportIssuingAuthorityStaff.setText(strPassportIssuingAuthorityStaff);
+                }
+                passportIssuingAuthorityStaff.setSelection(passportIssuingAuthorityStaff.getText().length());
+            }
+        });
+    }
 }
